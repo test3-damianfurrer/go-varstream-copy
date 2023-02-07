264a13dd062fd99beaf01ef11cb41f530e01caa3
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2023-02-08 00:49:55 +0100

    remove func & first concept try

--- listen.go
+++ listen.go
@@ -1,5 +1,5 @@
 package main
-
+#define S_TMPBUF 1024
 import (
     "fmt"
     "os"
@@ -7,52 +7,65 @@ import (
 //	"io"
 )
 
-func defStream(c net.Conn,out net.Conn,overlconn *net.Conn) {
+outs:=make(net.Conn,0) //maybe later handle with a stream copy socket, e.g. every (in/listen) output conn just (go routined) stream copies from a real output socket
+//"real out" = dial it, an out connection, instead of listener
+//a real out socket would be created from another program
+//we would need to pass this info to our program (maybe another socket :-))
+//we could also create a real out from and for this program.
+//go routines -> one connection to another go routine that distributes the data to every connection. 
+//this way we could just pass the out conn to the write functions. (instead of working with an array)
+
+func handleOut(lout net.Listener){
+	for {
+		conn, err = lout.Accept()
+		outs=append(v,conn)
+		if err != nil {
+			fmt.Println("accept error:", err.Error())
+		}
+		//while conn alive copy data
+		//nn.Close()
+		//nn=nil
+		//go echoServer(conn)
+	}
+}
+
+func defStream(cdef net.Conn,overlconn *net.Conn) {
+	for {
+		if *overlconn==nil {
+			tmpbuf = make([]byte, S_TMPBUF)
+			n, err := cdef.Read(tmpbuf)
+			if err != nil {
+				return //prob. req. program end
+			}
+			for i:=0;i<len(outs);i++ {
+				_ ,err = outs[i].Write(tmpbuf)
+				if err != nil {
+					ftm.Println("Conn write err",err)	
+				}
+			}
+		}	
+	}
  //while overlconn == nil -> copy c to out
 }
 
-func echoServer(c net.Conn) {
-    fmt.Printf("Client connected [%s]\n", c.RemoteAddr().Network())
-    fmt.Println("addr",c.RemoteAddr())
-    //io.Copy(c, c)
-	for{
-		databuf := make([]byte,0)
-		tmpbuf := make([]byte, 1)
-		
-		for {
-			_, err := c.Read(tmpbuf)
-			if err != nil {
-				if err.Error() != "EOF"{
-					fmt.Println("READ ERR",err.Error())
-				} else {
-					fmt.Println("Client Connection Closed",err.Error())
-				}
-				c.Close()
-				return
-					
-			}
-			//fmt.Println("byte",tmpbuf[0])
-			if tmpbuf[0] == '\n' {
-				databuf = append(databuf,'\n')
-				break
-			}
-			if tmpbuf[0] == 0 {
-				databuf = append(databuf,'\n')
-				break
-			}
-			if tmpbuf[0] == 10 {
-				databuf = append(databuf,'\n')
-				break
-			}
-			databuf = append(databuf,tmpbuf[0])
+func ovrlStream(covr net.Conn) {
+	for {
+		tmpbuf = make([]byte, S_TMPBUF)
+		n, err := covr.Read(tmpbuf)
+		if err != nil {
+			covr.Close()
+			covr=nil
+			return
 		}
-		fmt.Printf("Received: %s",databuf)
-		c.Write([]byte{'Y','o','u',' ','s','e','n','t',':',' '})
-		c.Write(databuf)
+		for i:=0;i<len(outs);i++ {
+			_ ,err = outs[i].Write(tmpbuf)
+			if err != nil {
+				ftm.Println("Conn write err",err)	
+			}
+		}	
 	}
-    c.Close()
-    fmt.Println("Connection Closed")
 }
+
 
 func main() {
 	mydir, err := os.Getwd()
@@ -81,6 +94,7 @@ func main() {
 	}
 	defer lovr.Close()
 
+	/*
 	SockAddr=mydir + "/out.sock"
 	if err := os.RemoveAll(SockAddr); err != nil {
 		panic(err)
@@ -91,24 +105,43 @@ func main() {
 		return
 	}
 	defer dout.Close()
+	*/
+	//out is a listen socket too, it just doesn't listen, but write instead.
+	SockAddr=mydir + "/out.sock"
+	if err := os.RemoveAll(SockAddr); err != nil {
+		panic(err)
+	}
+	lout, err := net.Listen("unix", SockAddr)
+	if err != nil {
+		fmt.Println("listen error:",err.Error())
+	}
+	defer lout.Close()
 
 	conn, err := ldef.Accept()
 	if err != nil {
 		fmt.Println("DEFAULT IN: accept error:", err.Error())
 		return
 	}
+	
+	go handleOut(lout)
+	
 	defconn:=conn
 	conn=nil
 	go defStream(defconn, dout, &conn)
 
 	for {
-		conn, err = lovr.Accept()
+		var conn2 net.Conn
+		conn2, err = lovr.Accept()
 		if err != nil {
-		fmt.Println("accept error:", err.Error())
+			fmt.Println("accept error:", err.Error())
 		}
+		if conn != nil {
+			conn.Close()
+		}
+		conn = conn2
+		go ovrlStream(conn)
 		//while conn alive copy data
-		conn.Close()
-		conn=nil
+		
 		//go echoServer(conn)
 	}
 }
